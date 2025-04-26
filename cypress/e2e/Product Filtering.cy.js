3f0b4508499dbffe8b3bb1290f42045205f9f9a9
rujal <rujal.dahal@featherwebs.com.np>
2025-04-26 13:38:52 +0545

    Added Product Browsing - Filtering changes

--- cypress/e2e/Product Filtering.cy.js
+++ cypress/e2e/Product Filtering.cy.js
@@ -1,4 +1,27 @@
 import { faker } from '@faker-js/faker';
+
 describe('Product Browsing - Filtering', () => {
+    it('Navigates, filters, clicks product, and verifies details', () => {
+        cy.visit('https://automationexercise.com/products');
     
-});
+        // Click on 'Women' category and then 'Dress'
+        cy.contains('Women').click();
+        cy.contains('Dress').click();
+    
+        // Check that filtered products contain at least one expected keyword
+        cy.get('.features_items').should('contain.text', 'Dress');
+
+        // Wait until products are loaded
+        cy.get('.features_items .product-image-wrapper').should('exist');
+
+        // Click the first product
+        cy.get('.features_items .product-image-wrapper').first().click();
+    
+        // Verify product details
+            cy.get("a[href='/product_details/3']").click()
+            cy.get('h2').should('be.visible'); // Product name
+            cy.get('span > span').should('contain.text', 'Rs'); // Product price
+            cy.contains('Availability: In Stock').should('be.visible');
+        });
+    });
+
